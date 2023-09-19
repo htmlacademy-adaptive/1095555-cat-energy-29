45455bb744cafe21cd222e1037d026431ce09044
Artem-D1 <artemdeobald123@gmail.com>
2023-09-19 17:28:04 +0700

    Внести правки после проверки наставника

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+const navToogle = document.querySelector(".main-nav__toogle");
+const nav = document.querySelector('.main-nav');
+
+// Убираем сразу с body класс no-js
+if (document.querySelector('.no-js')) {
+  document.querySelector('.no-js').classList.remove('no-js');
+}
+
+// Меняем класс у nav
+function showNav() {
+  nav.classList.toggle('main-nav--open');
+}
+
+// Проверяем, есть ли такой элемент на странице
+if (navToogle) {
+  navToogle.addEventListener('click', showNav);
+}
